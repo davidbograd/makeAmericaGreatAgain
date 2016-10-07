1a05cd3778265dcce27153d7f804093274dc3a42
louisebrandrup <louise.brandrup@hotmail.com>
2016-10-07 15:05:47 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,138 +1,109 @@
 
 
-function playRandomSound(){
-     // var sounds = ["givetrumpsomewaterplz.wav",
-     //  "trumpbeingtrump2.wav", 
-     //  "trumpbeingtrump3.wav",
-     //  "trumpbeingtrump4.wav",
-     //  "trumpbeingtrump5.wav",
-     //  "trumpbeingtrump6.wav",
-     //  "trumpbeingtrump7.wav",
-     //  "trumpbeingtrump8.wav",
-     //  "trumpbeingtrump9.wav",
-     //  "trumpbeingtrump10.wav",
-     //  "trumpbeingtrump11.wav",
-     //  "trumpbeingtrump12.wav",
-     //  "trumpbeingtrump13.wav",
-     //  "trumpbeingtrump14.wav",
-     //  "trumpbeingtrump15.wav",
-     //  "trumpbeingtrump16.wav",
-     //  "trumpbeingtrump17.wav",
-     //  "trumpbeingtrump18.wav",
-     //  "trumpbeingtrump19.wav",
-     //  "trumpbeingtrump20.wav",
-     //  "trumpbeingtrump21.wav",
-     //  "trumpbeingtrump22.wav",
-     //  "trumpbeingtrump23.wav",
-     //  "trumpbeingtrump24.wav",
-     //  "trumpbeingtrump25.wav",
-     //  "trumpbeingtrump26.wav"
-
-     //    ];
 
         var sounds = [
         {
-          "file": "givetrumpsomewaterplz.wav",
+          "file": "audio/givetrumpsomewaterplz.wav",
           "text": "...Hello? Can I get some water????..."
         },
         {
-          "file": "trumpbeingtrump2.wav",
+          "file": "audio/trumpbeingtrump2.wav",
           "text": "Bing, bing, bing..."
         },
         {
-          "file": "trumpbeingtrump3.wav",
+          "file": "audio/trumpbeingtrump3.wav",
           "text": "Bing, bing, bong, bong, bing..."
         },
         {
-          "file": "trumpbeingtrump4.wav",
+          "file": "audio/trumpbeingtrump4.wav",
           "text": "We can't sell beef...it's peanuts!"
         },
         {
-          "file": "trumpbeingtrump5.wav",
+          "file": "audio/trumpbeingtrump5.wav",
           "text": "I said it's ILLEGAL!"
         },
         {
-          "file": "trumpbeingtrump6.wav",
+          "file": "audio/trumpbeingtrump6.wav",
           "text": "You have to get hit with a kumatsu tractor..."
         },
         {
-          "file": "trumpbeingtrump7.wav",
+          "file": "audio/trumpbeingtrump7.wav",
           "text": "So what I'm gonna do is SPAN OUT, span out!"
         },
         {
-          "file": "trumpbeingtrump8.wav",
+          "file": "audio/trumpbeingtrump8.wav",
           "text": "I would never give up my microphone! I thought that was discusting!"
         },
         {
-          "file": "trumpbeingtrump9.wav",
+          "file": "audio/trumpbeingtrump9.wav",
           "text": "Terrible paper...I love it."
         },
         {
-          "file": "trumpbeingtrump10.wav",
+          "file": "audio/trumpbeingtrump10.wav",
           "text": "WHO is Uma married to?"
         },
         {
-          "file": "trumpbeingtrump11.wav",
+          "file": "audio/trumpbeingtrump11.wav",
           "text": "Because we're going to hell!"
         },
         {
-          "file": "trumpbeingtrump12.wav",
+          "file": "audio/trumpbeingtrump12.wav",
           "text": "Uhmm...we'll do something. We'll go to sleep first."
         },
         {
-          "file": "trumpbeingtrump13.wav",
+          "file": "audio/trumpbeingtrump13.wav",
           "text": "Our country is going to hell."
         },
         {
-          "file": "trumpbeingtrump14.wav",
+          "file": "audio/trumpbeingtrump14.wav",
           "text": "I beat the people from China, I win!"
         },
         {
-          "file": "trumpbeingtrump15.wav",
+          "file": "audio/trumpbeingtrump15.wav",
           "text": "We're tired of the nice people!"
         },
         {
-          "file": "trumpbeingtrump16.wav",
+          "file": "audio/trumpbeingtrump16.wav",
           "text": "Arab name, arab name, arab name"
         },
         {
-          "file": "trumpbeingtrump17.wav",
+          "file": "audio/trumpbeingtrump17.wav",
           "text": "What are you trying to talk to some of us about joining your little home? "
         },
         {
-          "file": "trumpbeingtrump18.wav",
+          "file": "audio/trumpbeingtrump18.wav",
           "text": "He's sweating like a pig! I've never saw a guy sweat like this!"
         },
          {
-          "file": "trumpbeingtrump19.wav",
+          "file": "audio/trumpbeingtrump19.wav",
           "text": "He wants to play golf! I play golf, i kill him at golf!"
         },
          {
-          "file": "trumpbeingtrump20.wav",
+          "file": "audio/trumpbeingtrump20.wav",
           "text": "Bing, bing, bing, right!?"
         },
          {
-          "file": "trumpbeingtrump21.wav",
+          "file": "audio/trumpbeingtrump21.wav",
           "text": "Oy, oy, oyee..."
         },
          {
-          "file": "trumpbeingtrump22.wav",
+          "file": "audio/trumpbeingtrump22.wav",
           "text": "I will take care of women, I respect women. I will take take of women."
         },
          {
-          "file": "trumpbeingtrump23.wav",
+          "file": "audio/trumpbeingtrump23.wav",
           "text": "You know, you could see there where blood coming out of her eyes...uh... blood coming out of her...wherever."
         },
          {
-          "file": "trumpbeingtrump24.wav",
+          "file": "audio/trumpbeingtrump24.wav",
           "text": "He put glasses on so people will think he is smart!"
         },
          {
-          "file": "trumpbeingtrump25.wav",
+          "file": "audio/trumpbeingtrump25.wav",
           "text": "You know, i have many millions between facebook and twitter, it's great! It's like owning a newspaper but without the losses, it's incredible!"
         },
          {
-          "file": "trumpbeingtrump25.wav",
+          "file": "audio/trumpbeingtrump25.wav",
           "text": "Rick Perry should have to have an IQ-test!"
         },
 
