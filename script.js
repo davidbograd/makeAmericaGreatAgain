14c6ee8828ec331ef6fc2ebcba1b516794d2fea7
MikaelSkoglund <mikael.e.skoglund@gmail.com>
2016-10-10 17:45:54 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,24 @@
+function onReady(callback) {
+    var intervalID = window.setInterval(checkReady, 2000);
+
+    function checkReady() {
+        if (document.getElementsByTagName('body')[0] !== undefined) {
+            window.clearInterval(intervalID);
+            callback.call(this);
+        }
+    }
+}
+
+function show(id, value) {
+    document.getElementById(id).style.display = value ? 'block' : 'none';
+}
+
+onReady(function () {
+    show('page', true);
+    show('loading', false);
+
+});
+
 function playRandomSound() {
 
     var sounds = [{
